ee508b1995f2c6b8bc3345b25c597324b0e09516
Adnane Hamid <adnane.hamid@student.ecp.fr>
2020-05-31 16:14:15 +0200

    Update cell_counting.py

--- cell_counting.py
+++ cell_counting.py
@@ -8,83 +8,131 @@ Created on Thu Apr 16 17:17:27 2020
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from cv2 import adaptiveThreshold
 
-def load_image(filename):
-    '''returns the image in the filename from the datatset'''
-    img = cv2.imreadmulti(filename)
-    img = img[1][0]
-    #plt.imshow(img)
-    return(img)
+'''Example images used in the report'''
+#filename = './Dataset/S07-33-MAX_Dm-Kr walking No 182 - NC14.lif - overview.tif'
+filename = './Dataset/S04-04-MAX_Dm-Kr length No 193.lif - overview.tif'
+#filename = './Dataset/S05-05-MAX_Dm-Kr mRNA B2 B3 seperate probes 3h Amp.lif - overview.tif'
+#filename = './Dataset/S06-06-MAX_Dm-Kr mRNA B2 B3 seperate probes ON Amp.lif - overview.tif'
+#filename = './Dataset/S01-34-MAX_Dm-Kr walking No 182.lif - overview embryo1.tif'
 
-def remove_zero_rows(img):
-    inc = 0
-    while np.all(img[inc]==0):
-        inc+=1
-    inc -=1
 
-#filename = "C:/Users/adnane/Desktop/Dataset/S01-34-MAX_Dm-Kr walking No 182.lif - overview embryo1.tif"
-#filename = 'C:/Users/adnane/Desktop/Dataset/S02-18-MAX_Dm-Kr walking No 179.lif - 19-12-06 - overview.tif'
-#filename = './Dataset/S03-19-MAX_Dm-Kr walking No 179.lif - overview.tif'
+def cell_counter(filename, plot=False, n=100, m=100):
+    '''Returns the number of cells inside the window of the image defined by
+    n for height and m for width'''
 
-def cell_counter(filename):
+    def load_image(filename):
+        '''returns the image in the filename from the datatset'''
+        img = cv2.imreadmulti(filename)
+        img = img[1][0]
+        return(img)
+
     img = load_image(filename)
-    
-    
-    mask = np.where(img<50)
-    
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    img = clahe.apply(img)
-    #plt.imshow(img)
-    
-    th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,25,1)
-    #plt.imshow(th2)
-    
-    th2[mask] = 0
-    
-    #plt.imshow(th2)
-    
-    median = cv2.medianBlur(th2,7)
-    
-    
-    opening = median
-    #blur = median
-    #ret3,th4 = _, median
-    #plt.imshow(opening)
-    
-    
-    # sure background area
-    sure_bg = median.copy()
-    # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-    #plt.imshow(dist_transform)
-    
-    ret, sure_fg = cv2.threshold(dist_transform,0.4*dist_transform.max(),255,0)
-    #plt.imshow(sure_fg)
-    #plt.imshow(sure_bg)
-    
-    
+
+    witness = img.copy()
+
+    '''Initial background mask using a safe global threshold at 20'''
+    th = 20
+    mask = np.where(img < th)
+
+    '''Denoising median blur'''
+    img = cv2.medianBlur(img, 5)
+
+    '''Mean Adaptive thresholding '''
+    img = cv2.adaptiveThreshold(
+        img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51, 0)
+
+    '''Applying the initial background mask'''
+    img[mask] = 0
+
+    '''Median blur as a post processing noise removal'''
+    img = cv2.medianBlur(img, 9)
+
+    img = img[img.shape[0] // 2 - n: img.shape[0] // 2 +
+              n, img.shape[1] // 2 - m: img.shape[1] // 2 + m]
+
+    if plot:
+        plt.title('Mean adaptive thresholding')
+        plt.imshow(img)
+        plt.show()
+
+    witness = witness[witness.shape[0] // 2 - n: witness.shape[0] //
+                      2 + n, witness.shape[1] // 2 - m: witness.shape[1] // 2 + m]
+
+    witness = cv2.cvtColor(witness, cv2.COLOR_GRAY2RGB)
+
+    # =============================================================================
+    # watershed
+    # =============================================================================
+
+    '''Distance transform'''
+    sure_bg = img.copy()
+
+    dist_transform = cv2.distanceTransform(img, cv2.DIST_L2, 5)
+
+    if plot:
+        plt.title('Distance transform')
+        plt.imshow(dist_transform)
+        plt.show()
+
+    '''Watershed to separate touching cells'''
+    ret, sure_fg = cv2.threshold(
+        dist_transform, 0.5 * dist_transform.max(), 255, 0)
+
     sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg,sure_fg)
-    
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
     ret, markers = cv2.connectedComponents(sure_fg)
-    markers = markers+1
-    markers[unknown==255] = 0
+    markers = markers + 1
+    markers[unknown == 255] = 0
     markers = markers.astype('int32')
-    
-    #now load same image as color image
-    img = cv2.cvtColor(median, cv2.COLOR_GRAY2RGB)
-    
-    markers = cv2.watershed(img,markers)
-    img[markers == -1] = [0,0,0]
-    #plt.imshow(img)
-    
+
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [0, 0, 0]
+
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, markers = cv2.connectedComponents(img)
-    
-    #print( str(ret) + ' cells')
-    return ret
-    
+
+    '''Bitwise and median blur to remove salt noise'''
+    median = cv2.medianBlur(img, 5)
+    img = np.bitwise_and(median, img)
+
+    if plot:
+        plt.title('Original')
+        plt.imshow(witness)
+        plt.show()
+
+    if plot:
+        plt.title('Final counted connected components')
+        plt.imshow(img)
+        plt.show()
+
+    num_cells, labels_im = cv2.connectedComponents(img, connectivity=4)
+    print('There are ' + str(num_cells - 1) +
+          ' cells in an area of ' + str(2 * n * 2 * m) + ' pixels')
+
+    def imshow_components(labels):
+        # Map component labels to hue val
+        label_hue = np.uint8(179 * labels / np.max(labels))
+        blank_ch = 255 * np.ones_like(label_hue)
+        labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+        # cvt to BGR for display
+        labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+        # set bg label to black
+        labeled_img[label_hue == 0] = 0
+
+        cv2.imshow('labeled.png', labeled_img)
+        cv2.waitKey()
+        return(labeled_img)
+
+    if plot:
+        imshow_components(labels_im)
+
+    return(num_cells - 1)
+
+
 if __name__ == "__main__":
-    filename = './Dataset/S03-19-MAX_Dm-Kr walking No 179.lif - overview.tif'
-    print("number of cells:" + str(cell_counter(filename)))
+    count = cell_counter(filename, plot=True)
